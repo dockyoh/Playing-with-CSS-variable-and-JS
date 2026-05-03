4ee3ce9fbba9c7745e0043b21b4cec146d8a701b
Reygin <sreygin@gmail.com>
2026-05-03 20:26:29 +0800

    Code V2 using forEach() method

--- cssVariable.js
+++ cssVariable.js
@@ -1,3 +1,5 @@
+const inputEl = document.querySelectorAll("input");
+
 const spacingInputEl = document.querySelector("#spacing-label");
 const blurInputEl = document.querySelector("#blur-label");
 const colorInputEl = document.querySelector("#color-label");
@@ -7,18 +9,17 @@ const spanEl = document.querySelector("span");
 
 const imgEl = document.querySelector("img");
 
-spacingInputEl.addEventListener("input", (event) => {
-  let spacingValue = event.target.value;
-  imgEl.style.setProperty(`--spacing`, `${spacingValue}px`);
-});
-
-blurInputEl.addEventListener("input", (event) => {
-  let blurValue = event.target.value;
-  imgEl.style.setProperty(`--blur`, `${blurValue}px`);
-});
-
-colorInputEl.addEventListener("input", (event) => {
-  let colorValue = event.target.value;
-  imgEl.style.setProperty(`--base-color`, `${colorValue}`);
-  spanEl.style.setProperty(`--base-color`, `${colorValue}`);
+inputEl.forEach((input) => {
+  input.addEventListener("input", (event) => {
+    const inputValue = event.target.value;
+    const inputName = event.target.name;
+    if (inputName === "spacing-input") {
+      imgEl.style.setProperty(`--spacing`, `${inputValue}px`);
+    } else if (inputName === "blur-input") {
+      imgEl.style.setProperty(`--blur`, `${inputValue}px`);
+    } else {
+      imgEl.style.setProperty(`--base-color`, `${inputValue}`);
+      spanEl.style.setProperty(`--base-color`, `${inputValue}`);
+    }
+  });
 });
