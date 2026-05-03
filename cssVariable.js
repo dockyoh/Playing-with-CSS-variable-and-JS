13a617b0e0ea10a01be59d625f350959c8c8feb9
Reygin <sreygin@gmail.com>
2026-05-03 21:49:02 +0800

    Code V3 using global scope

--- cssVariable.js
+++ cssVariable.js
@@ -1,25 +1,15 @@
 const inputEl = document.querySelectorAll("input");
-
-const spacingInputEl = document.querySelector("#spacing-label");
-const blurInputEl = document.querySelector("#blur-label");
 const colorInputEl = document.querySelector("#color-label");
 colorInputEl.value = "#ffffff";
 
-const spanEl = document.querySelector("span");
-
-const imgEl = document.querySelector("img");
-
 inputEl.forEach((input) => {
   input.addEventListener("input", (event) => {
+    const unitValue = event.target.dataset.unit || "";
     const inputValue = event.target.value;
     const inputName = event.target.name;
-    if (inputName === "spacing-input") {
-      imgEl.style.setProperty(`--spacing`, `${inputValue}px`);
-    } else if (inputName === "blur-input") {
-      imgEl.style.setProperty(`--blur`, `${inputValue}px`);
-    } else {
-      imgEl.style.setProperty(`--base-color`, `${inputValue}`);
-      spanEl.style.setProperty(`--base-color`, `${inputValue}`);
-    }
+    document.documentElement.style.setProperty(
+      `--${inputName}`,
+      `${inputValue}${unitValue}`,
+    );
   });
 });
